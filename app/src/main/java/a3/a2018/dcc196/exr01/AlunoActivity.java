85ea23ce7fc628ea3a80a69ea2daf4e4a7413dc7
João Pedro <pedrocxm@gmail.com>
2018-09-05 20:25:18 -0300

    telas secundarias criadas

---
+++ app/src/main/java/a3/a2018/dcc196/exr01/AlunoActivity.java
@@ -0,0 +1,4 @@
+package a3.a2018.dcc196.exr01;
+
+public class AlunoActivity {
+}
---
+++ app/src/main/java/a3/a2018/dcc196/exr01/ProfessorActivity.java
@@ -0,0 +1,13 @@
+package a3.a2018.dcc196.exr01;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ProfessorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

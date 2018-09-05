75a195dc42a01017af733b3f57c2e58546560249
João Pedro <pedrocxm@gmail.com>
2018-09-05 20:56:29 -0300

    Listeners dos botões da tela inicial criada

--- app/src/main/java/a3/a2018/dcc196/exr01/ServidorActivity.java
+++ app/src/main/java/a3/a2018/dcc196/exr01/ServidorActivity.java
@@ -3,7 +3,7 @@ package a3.a2018.dcc196.exr01;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class ProfessorActivity extends AppCompatActivity {
+public class ServidorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
